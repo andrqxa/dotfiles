0f63b0b0b7f49550406b99fd0a7dc662744ac33b
Andrii Puhachenko <andrqxa@gmail.com>
2023-12-06 08:48:27 -0500

    Update go-install.sh

--- Go/go-install.sh
+++ Go/go-install.sh
@@ -12,19 +12,12 @@ echo $GO_PATH
 
 ## Remove old version if it exists
 rm -rf $GO_HOME/go
-ls -la $GO_HOME
 
 ## Create folder for programm
-mkdir -p $GO_HOME && chown -R $USR:$USR $GO_HOME
-ls -la $GO_HOME
-
-
+mkdir -p $GO_HOME
 mkdir -p $GO_PATH/src
 mkdir -p $GO_PATH/pkg
 mkdir -p $GO_PATH/bin
-chown -R $USR:$USR $PROJECTS
-
-ls -la $GO_PATH
 
 ## Detect the user's shell and add the appropriate path variables
 SHELL_TYPE=$(basename "$SHELL")
@@ -57,10 +50,10 @@ echo "Extraction complete"
 echo $SHELL_RC
 if [ -z "${GOROOT}" ]; then
   echo "export GOROOT=$GO_HOME/go" >> "$SHELL_RC"
-  echo "export PATH=$GOROOT/bin:$PATH" >> "$SHELL_RC"
+  echo "export PATH=\$GOROOT/bin:\$PATH" >> "$SHELL_RC"
 elif [ -z "${GOPATH}" ]; then
   echo "export GOPATH=$GO_PATH/go" >> "$SHELL_RC"
-  echo "export PATH=$GOPATH/bin:$PATH" >> "$SHELL_RC"
+  echo "export PATH=\$GOPATH/bin:\$PATH" >> "$SHELL_RC"
 fi
 
 ## Verify the installation
@@ -74,6 +67,12 @@ if [ -x "$(command -v go)" ]; then
 else
     echo "Go is not found in the PATH. Make sure to add Go's bin directory to your PATH."
 fi
+
+# Set permissions
+chown -R $USR:$USR $GO_HOME
+chown -R $USR:$USR $PROJECTS
+ls -la $GO_HOME
+ls -la $GO_PATH
 
 ## Clean up
 rm go$VERSION.linux-$ARCH.tar.gz
